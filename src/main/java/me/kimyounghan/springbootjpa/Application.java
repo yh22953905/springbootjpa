2a7457a554c29bfdaef9fcb0f50ed90a18ff68fc
KIMYOUNGHAN <yh22953905@gmail.com>
2020-06-16 22:33:56 +0900

    스프링 데이터 JPA - 핵심 개념 이해 > 관계형 데이터베이스와 자바

--- src/main/java/me/kimyounghan/springbootjpa/Application.java
+++ src/main/java/me/kimyounghan/springbootjpa/Application.java
@@ -1,13 +1,24 @@
 package me.kimyounghan.springbootjpa;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-@SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	public static void main(String[] args) throws SQLException {
+		String url = "jdbc:postgresql://localhost:5432/springdata";
+		String username = "younghan";
+		String password = "pass";
+
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+//			String sql = "CREATE TABLE ACCOUNT (id int, username varchar(255), password varchar(255));";
+			String sql = "INSERT INTO ACCOUNT VALUES (1, 'KIMYOUNGHAN', 'pass');";
+			try(PreparedStatement statement = connection.prepareStatement(sql)) {
+				statement.execute();
+			}
+		}
 	}
 
 }
