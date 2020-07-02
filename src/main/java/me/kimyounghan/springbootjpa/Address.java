d6ffa6b916d2ff0fc7ae3ae7c843302fdbc5bd24
KIMYOUNGHAN <yh22953905@gmail.com>
2020-07-02 22:41:13 +0900

    스프링 데이터 JPA - 핵심 개념 이해 > JPA 프로그래밍 3. Value 타입 맵핑

---
+++ src/main/java/me/kimyounghan/springbootjpa/Address.java
@@ -0,0 +1,16 @@
+package me.kimyounghan.springbootjpa;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zipCode;
+
+}
