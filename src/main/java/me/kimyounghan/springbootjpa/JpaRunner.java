05392f14ea033ca84f97eaefe5356384a92fb9ae
KIMYOUNGHAN <yh22953905@gmail.com>
2020-07-12 00:19:36 +0900

    스프링 데이터 JPA - 핵심 개념 이해 > JPA 프로그래밍 7. 쿼리 -- Criteria

--- src/main/java/me/kimyounghan/springbootjpa/JpaRunner.java
+++ src/main/java/me/kimyounghan/springbootjpa/JpaRunner.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Component;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -20,8 +23,12 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post AS p", Post.class);
-        List<Post> posts = query.getResultList();
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Post> query = builder.createQuery(Post.class);
+        Root<Post> root = query.from(Post.class);
+        query.select(root);
+
+        List<Post> posts = entityManager.createQuery(query).getResultList();
         posts.forEach(System.out::println);
     }
 
