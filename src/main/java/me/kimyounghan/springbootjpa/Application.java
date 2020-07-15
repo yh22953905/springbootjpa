b22366bbfe50ef5585f300e1cd55a2876f9a1887
KIMYOUNGHAN <yh22953905@gmail.com>
2020-07-15 23:02:09 +0900

    스프링 데이터 JPA - 핵심 개념 이해 > 스프링 데이터 JPA 원리

--- src/main/java/me/kimyounghan/springbootjpa/Application.java
+++ src/main/java/me/kimyounghan/springbootjpa/Application.java
@@ -2,8 +2,10 @@ package me.kimyounghan.springbootjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(YounghanRegistrar.class)
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/me/kimyounghan/springbootjpa/JpaRunner.java
+++ src/main/java/me/kimyounghan/springbootjpa/JpaRunner.java
@@ -1,16 +1,10 @@
 package me.kimyounghan.springbootjpa;
 
-import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -18,15 +12,12 @@ import java.util.List;
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
-    @PersistenceContext
-    EntityManager entityManager;
+    @Autowired
+    PostRepository postRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        List<Post> posts = entityManager
-                .createNativeQuery("SELECT * FROM POST", Post.class).getResultList();
-
-        posts.forEach(System.out::println);
+        postRepository.findAll().forEach(System.out::println);
     }
 
 }
