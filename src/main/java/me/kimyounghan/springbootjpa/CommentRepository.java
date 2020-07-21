3db43720ed0149385000398d357ed3a63dff3133
KIMYOUNGHAN <yh22953905@gmail.com>
2020-07-21 22:15:03 +0900

    스프링 데이터 JPA - 스프링 데이터 JPA 활용 > 스프링 데이터 Common 4. 쿼리 만들기 실습 -- 페이징

--- src/main/java/me/kimyounghan/springbootjpa/CommentRepository.java
+++ src/main/java/me/kimyounghan/springbootjpa/CommentRepository.java
@@ -3,18 +3,11 @@ package me.kimyounghan.springbootjpa;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CommentRepository extends MyRepository<Comment, Long> {
 
-    List<Comment> findByCommentContains(String keyword);
-
-    List<Comment> findByCommentContainsIgnoreCase(String keyword);
-
-    List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String keyword, int likeCount);
-
-    List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountDesc(String keyword);
+    Page<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
 
 }
--- src/test/java/me/kimyounghan/springbootjpa/CommentRepositoryTest.java
+++ src/test/java/me/kimyounghan/springbootjpa/CommentRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -23,8 +26,10 @@ public class CommentRepositoryTest {
         this.createComment(100, "spring data jpa");
         this.createComment(50, "HIBERNATE SPRING");
 
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseOrderByLikeCountDesc("Spring");
-        assertThat(comments.size()).isEqualTo(2);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "LikeCount"));
+
+        Page<Comment> comments = commentRepository.findByCommentContainsIgnoreCase("Spring", pageRequest);
+        assertThat(comments.getNumberOfElements()).isEqualTo(2);
         assertThat(comments).first().hasFieldOrPropertyWithValue("LikeCount", 100);
     }
 
