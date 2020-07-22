8b688bb880108651665cd94aaf4de9eaf17a8fbd
KIMYOUNGHAN <yh22953905@gmail.com>
2020-07-22 23:11:45 +0900

    스프링 데이터 JPA - 스프링 데이터 JPA 활용 > 스프링 데이터 Common 6. 비동기 쿼리 메소드 -- ListenableFuture => 역시 추천하지 않음. - 1

--- src/main/java/me/kimyounghan/springbootjpa/CommentRepository.java
+++ src/main/java/me/kimyounghan/springbootjpa/CommentRepository.java
@@ -3,14 +3,13 @@ package me.kimyounghan.springbootjpa;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.List;
-import java.util.concurrent.Future;
-import java.util.stream.Stream;
 
 public interface CommentRepository extends MyRepository<Comment, Long> {
 
     @Async
-    Future<List<Comment>> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
+    ListenableFuture<List<Comment>> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
 
 }
--- src/test/java/me/kimyounghan/springbootjpa/CommentRepositoryTest.java
+++ src/test/java/me/kimyounghan/springbootjpa/CommentRepositoryTest.java
@@ -4,19 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.lang.Nullable;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
-import java.sql.SQLOutput;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -32,13 +28,23 @@ public class CommentRepositoryTest {
 
         PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "LikeCount"));
 
-        Future<List<Comment>> future =
+        ListenableFuture<List<Comment>> future =
                 commentRepository.findByCommentContainsIgnoreCase("Spring", pageRequest);
         System.out.println("==========");
         System.out.println("is done? : " + future.isDone());
 
-        List<Comment> comments = future.get();
-        comments.forEach(System.out::println);
+        future.addCallback(new ListenableFutureCallback<List<Comment>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println(throwable);
+            }
+
+            @Override
+            public void onSuccess(@Nullable List<Comment> comments) {
+                System.out.println("==========");
+                System.out.println(comments);
+            }
+        });
     }
 
     private void createComment(int likeCount, String comment) {
