a2758bea07414031cd47960bdf89a31848677229
KIMYOUNGHAN <yh22953905@gmail.com>
2020-08-02 13:39:08 +0900

    스프링 데이터 JPA - 스프링 데이터 JPA 활용 > 스프링 데이터 Common 10. QueryDSL 연동 -- QuerydslJpaRepository is deprecated

--- src/main/java/me/kimyounghan/springbootjpa/PostRepository.java
+++ src/main/java/me/kimyounghan/springbootjpa/PostRepository.java
@@ -1,6 +1,7 @@
 package me.kimyounghan.springbootjpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface PostRepository extends MyRepository<Post, Long> {
+public interface PostRepository extends MyRepository<Post, Long>, QuerydslPredicateExecutor<Post> {
 }
--- src/test/java/me/kimyounghan/springbootjpa/PostRepositoryTest.java
+++ src/test/java/me/kimyounghan/springbootjpa/PostRepositoryTest.java
@@ -1,15 +1,16 @@
 package me.kimyounghan.springbootjpa;
 
+import com.querydsl.core.types.Predicate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -19,31 +20,15 @@ public class PostRepositoryTest {
     @Autowired
     PostRepository postRepository;
 
-    @Autowired
-    ApplicationContext applicationContext;
-
-    @Test
-    public void event() {
-        Post post = new Post();
-        post.setTitle("event");
-        PostPublishedEvent event = new PostPublishedEvent(post);
-
-        applicationContext.publishEvent(event);
-    }
-
     @Test
     public void crud() {
         Post post = new Post();
         post.setTitle("hibernate");
-
-        assertThat(postRepository.contains(post)).isFalse();
-
         postRepository.save(post.publish());
 
-        assertThat(postRepository.contains(post)).isTrue();
-
-        postRepository.delete(post);
-        postRepository.flush();
+        Predicate predicate = QPost.post.title.containsIgnoreCase("Hiber");
+        Optional<Post> one = postRepository.findOne(predicate);
+        assertThat(one).isNotEmpty();
     }
 
 }
