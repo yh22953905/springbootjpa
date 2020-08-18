06a6955f06577aedf24983ace959909dc25fc492
KIMYOUNGHAN <yh22953905@gmail.com>
2020-08-18 22:13:16 +0900

    스프링 데이터 JPA - 스프링 데이터 JPA 활용 > 스프링 데이터 Common 13. 웹 기능 3부 Pageable과 Sort

--- src/main/java/me/kimyounghan/springbootjpa/PostController.java
+++ src/main/java/me/kimyounghan/springbootjpa/PostController.java
@@ -1,20 +1,25 @@
 package me.kimyounghan.springbootjpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 public class PostController {
 
     @Autowired
-    private PostRepository postRepository;
+    private PostRepository posts;
 
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable("id") Post post) {
         return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public Page<Post> getPost(Pageable pageable) {
+        return posts.findAll(pageable);
     }
 }
--- src/test/java/me/kimyounghan/springbootjpa/PostControllerTest.java
+++ src/test/java/me/kimyounghan/springbootjpa/PostControllerTest.java
@@ -9,10 +9,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -39,4 +39,20 @@ public class PostControllerTest {
         ;
     }
 
+    @Test
+    public void getPosts() throws Exception {
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/")
+                .param("page", "0")
+                .param("size", "10")
+                .param("sort", "created,desc")
+                .param("sort", "title"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.content[0].title", is("jpa")))
+        ;
+    }
 }
