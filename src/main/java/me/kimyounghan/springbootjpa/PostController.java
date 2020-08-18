daa76b9276dac940df5b832996f8eb39e294e740
KIMYOUNGHAN <yh22953905@gmail.com>
2020-08-19 00:19:32 +0900

    스프링 데이터 JPA - 스프링 데이터 JPA 활용 > 스프링 데이터 Common 14. 웹 기능 4부 HATEOAS

--- src/main/java/me/kimyounghan/springbootjpa/PostController.java
+++ src/main/java/me/kimyounghan/springbootjpa/PostController.java
@@ -3,6 +3,9 @@ package me.kimyounghan.springbootjpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +22,7 @@ public class PostController {
     }
 
     @GetMapping("/posts")
-    public Page<Post> getPost(Pageable pageable) {
-        return posts.findAll(pageable);
+    public PagedModel<EntityModel<Post>> getPost(Pageable pageable, PagedResourcesAssembler<Post> assembler) {
+        return assembler.toModel(posts.findAll(pageable));
     }
 }
--- src/test/java/me/kimyounghan/springbootjpa/PostControllerTest.java
+++ src/test/java/me/kimyounghan/springbootjpa/PostControllerTest.java
@@ -41,12 +41,10 @@ public class PostControllerTest {
 
     @Test
     public void getPosts() throws Exception {
-        Post post = new Post();
-        post.setTitle("jpa");
-        postRepository.save(post);
+        createPosts();
 
         mockMvc.perform(get("/posts/")
-                .param("page", "0")
+                .param("page", "3")
                 .param("size", "10")
                 .param("sort", "created,desc")
                 .param("sort", "title"))
@@ -54,5 +52,16 @@ public class PostControllerTest {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.content[0].title", is("jpa")))
         ;
+    }
+
+    private void createPosts() {
+        int postsCount = 100;
+
+        while (postsCount > 100) {
+            Post post = new Post();
+            post.setTitle("jpa");
+            postRepository.save(post);
+            postsCount--;
+        }
     }
 }
