ba2b7b820aab9f0aae9884d35bcdb440432ebef1
KIMYOUNGHAN <yh22953905@gmail.com>
2021-04-26 23:31:04 +0900

    스프링 데이터 JPA - 스프링 데이터 JPA 활용 > 스프링 데이터 JPA 6. Update 쿼리

--- src/main/java/me/kimyounghan/springbootjpa/PostRepository.java
+++ src/main/java/me/kimyounghan/springbootjpa/PostRepository.java
@@ -2,8 +2,8 @@ package me.kimyounghan.springbootjpa;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -17,4 +17,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query(value = "SELECT p FROM #{#entityName} AS p WHERE p.title = :title")
     List<Post> findByTitle2(@Param("title") String keyword, Sort sort);
 
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "UPDATE Post p SET p.title = ?1 WHERE p.id =?2")
+    int updateTitle(String title, Long id);
 }
--- src/test/java/me/kimyounghan/springbootjpa/PostRepositoryTest.java
+++ src/test/java/me/kimyounghan/springbootjpa/PostRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -64,10 +65,23 @@ public class PostRepositoryTest {
         assertThat(all.size()).isEqualTo(1);
     }
 
-    private void savePost() {
+    private Post savePost() {
         Post post = new Post();
         post.setTitle("Spring");
-        postRepository.save(post);
+        return postRepository.save(post); // persist
+    }
+
+    @Test
+    public void updateTitle() {
+        Post spring = savePost();
+
+        String hibernate = "hibernate";
+        int update = postRepository.updateTitle(hibernate, spring.getId());
+        assertThat(update).isEqualTo(1);
+
+        Optional<Post> byId = postRepository.findById(spring.getId());
+        assertThat(byId.get().getTitle()).isEqualTo(hibernate);
+        // clear 또는 flush 해주지 않으면, spring 변수에 해당하는 Post 객체가 여전히 Persistent Context 에 남아 있기 때문에 select 를 하지 않아 update 가 일어나지 않음.
     }
 
 }
