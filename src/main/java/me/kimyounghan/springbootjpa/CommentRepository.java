b22a9d206d22130887cb670ea9d45f732f8cc0aa
KIMYOUNGHAN <yh22953905@gmail.com>
2021-05-01 23:28:11 +0900

    스프링 데이터 JPA - 스프링 데이터 JPA 활용 > 스프링 데이터 JPA 11. 트랜잭션

--- src/main/java/me/kimyounghan/springbootjpa/CommentRepository.java
+++ src/main/java/me/kimyounghan/springbootjpa/CommentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +16,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpec
     Optional<Comment> getById(Long id);
 
     // Closed Projection
+    @Transactional(readOnly = true)  // flush 모드를 NEVER 로 설정하여, Dirty Checking 을 하지 않도록 함.
     <T> List<T> findByPost_Id(Long id, Class<T> type);
 
 }
